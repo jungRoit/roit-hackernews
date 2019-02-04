6ea6ab306223aa80ab4f41b79b3db3d3c86ef82a
jungRoit <roit.jung@gmail.com>
2019-02-04 16:24:57 +0545

    pagination shifted to story-wrapper component

--- src/components/newstories/index.js
+++ src/components/newstories/index.js
@@ -1,64 +1,21 @@
 import React from 'react';
 import StoryWrapper from '../story-wrapper';
-import * as API from '../../services/api.js';
-import './index.css';
 
 /**
- * Component for New Stories.
+ * Component for top Stories.
  */
-class NewStories extends React.Component {
+const NewStories = () => {
 
   /**
    * 
-   * @param {*} props 
    */
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      stories: [],
-      pageNo: 1,
-      itemsPerPage: 20
-    };
-  }
+  return (
+    <StoryWrapper
+      key='newstories'
+      storyType='newstories'
+    />
+  );
 
-  /**
-   * Function to call API and get new stories.
-   */
-  componentDidMount() {
-    API.getStoryList('newstories')
-      .then(res => this.setState({ isLoaded: true, stories: res.data }))
-      .catch(err => err);
-  }
-
-  loadMoreStories = () => {
-    this.setState({ pageNo: this.state.pageNo + 1 });
-  }
-
-  /**
-   * Function to render jsx.
-   */
-  render() {
-    return (
-      this.state.isLoaded
-        ? <div>
-          <StoryWrapper
-            key='newstories'
-            stories={this.state.stories.slice(
-              0,
-              this.state.pageNo * this.state.itemsPerPage)}
-          />
-          <div
-            className='pagination'
-            onClick={this.loadMoreStories}
-          >
-            <p>Load More</p>
-          </div>
-        </div>
-        : <h2>Loadiing...</h2>
-
-    );
-  }
 }
 
 export default NewStories;
--- src/components/story-wrapper/index.js
+++ src/components/story-wrapper/index.js
@@ -1,34 +1,71 @@
 import React from 'react';
 import Story from '../story';
 import './index.css';
+import * as API from '../../services/api';
 
 /**
  * Stateless Component to wrap all stories.
  * 
- * @param {*} props 
  */
-const StoryWrapper = (props) => {
+class StoryWrapper extends React.Component {
+  /**
+   * 
+   * @param {*} props 
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: [],
+      pageNo: 1,
+      itemsPerPage: 20,
+      isLoaded: false
+    }
+  }
+
+  /**
+   * Function to get stories.
+   */
+  componentDidMount() {
+    API.getStoryList(this.props.storyType)
+      .then(res => this.setState({ isLoaded: true, stories: res.data }))
+      .catch(err => err);
+  }
+
+  loadMoreStories = () => {
+    this.setState({ pageNo: this.state.pageNo + 1 });
+  }
 
   /**
    * 
    */
-  return (
-    <div className='story-wrapper'>
-      {(props.stories.length !== 0) ?
-        props.stories.map(id =>
-          <Story
-            key={id}
-            id={id}
-          />
-        )
-        : <div>
-          <h1>No Stories</h1>
+  render() {
+    return (
+      <div className='story-wrapper'>
+        <div>
+          {this.state.stories.slice(
+            0,
+            this.state.pageNo * this.state.itemsPerPage)
+            .map(id =>
+              <Story
+                key={id}
+                id={id}
+              />
+            )}
+          <div
+            className='pagination'
+            onClick={this.loadMoreStories}
+          >
+            <p>Load More Stories</p>
+          </div>
         </div>
-      }
 
-    </div>
-  );
-};
+        }
+
+      </div>
+    );
+  }
+
+}
 
 
 export default StoryWrapper;
